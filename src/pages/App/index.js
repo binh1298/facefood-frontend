eb46dabce87d0e380e753be3ae1ad37d0dc2a5cf
ducphi <48780427+DucPhinoDokku@users.noreply.github.com>
2020-02-20 11:29:47 +0700

    update route

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,41 +1,20 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Login from '../Login';
-import Home from '../Home';
-import Register from '../Register';
-import ListUser from '../ListUser';
+import { BrowserRouter} from 'react-router-dom';
 import Navigation from '../../components/Navigation';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from '../../utils/theme';
-import UserDetail from '../UserDetail';
 import { Routes } from '../../routes';
+import { Toolbar } from '@material-ui/core';
 
 class App extends React.Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
+        <Toolbar></Toolbar>
         <BrowserRouter>
           <Navigation />
-          <div>
-            <Switch>
-              <Route path="/login">
-                <Login />
-              </Route>
-              <Route path="/register">
-                <Register />
-              </Route>
-              <Route path="/listUser">
-                <ListUser />
-              </Route>
-              <Route path="/user">
-               <UserDetail/>
-              </Route>
-              <Route path="/">
-                <Home />
-              </Route>
-            </Switch>
-          </div>
+          {Routes}
         </BrowserRouter>
       </ThemeProvider>
     );
