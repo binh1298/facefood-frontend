b6c0b731b479aadf0de9bf20ec8d4b643b51e13f
ducphi <48780427+DucPhinoDokku@users.noreply.github.com>
2020-02-21 08:49:17 +0700

    Update UI

--- src/pages/ListUser/index.js
+++ src/pages/ListUser/index.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment, useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -9,7 +9,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, FormControl, InputLabel, NativeSelect, TablePagination, Container, Typography } from '@material-ui/core';
+import {TablePagination, Container, Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { get } from '../../utils/ApiCaller';
 import searchBar from '../../components/UserSearchBar/index.js';
