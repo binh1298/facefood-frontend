d8e5fc66207b560a78a4c2c1e6d92a22bd3e0562
hongduc0206 <hongduc.j@gmail.com>
2020-03-16 23:42:44 +0700

    update UI

--- src/pages/UserDetail/index.js
+++ src/pages/UserDetail/index.js
@@ -1,18 +1,14 @@
-import { Container, makeStyles, TextField, Typography } from '@material-ui/core';
+import { Button, Container, makeStyles, Typography } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
-import IconButton from '@material-ui/core/IconButton';
-import Paper from '@material-ui/core/Paper';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { get } from '../../utils/ApiCaller';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import InfoGrid from '../../components/InfoGridComponent/InfoGrid';
-import TimerIcon from '@material-ui/icons/Timer';
+import { get } from '../../utils/ApiCaller';
 const useStyles = makeStyles(theme => ({
   root: {
     marginTop: '10px',
@@ -75,35 +71,32 @@ export default function UserDetail() {
       });
   }, []);
 
- 
+
 
   function handleClickIcon() {
 
   }
   const status = "Active";
-  let action = <Typography className={classes.banButton}>Ban</Typography>;
+  let action = 'Ban';
   if (userData.isDeleted === true) {
-    action = <Typography className={classes.activatedButton}>Activated</Typography>;
+    action = 'Activated'
     status = "Banned";
   }
 
   return (
     <Container className={classes.root}>
       <Grid container spacing={1} className={classes.containerName}>
-        <Grid item xs={2} className={classes.typoName} >
+        <Grid item xs={3} className={classes.typoName} >
           <Typography>{userData.fullname}</Typography>
         </Grid>
-        <Grid item xs={9}>
-          <IconButton onClick={handleClickIcon}>
+        <Grid item xs={8}>
+        <Button
+            variant="contained"
+            color="secondary"
+            onClick={handleClickIcon}
+          >
             {action}
-          </IconButton>
-        </Grid>
-        <Grid item xs={1} style={{ float: "right" }}>
-          <Link to="/users">
-            <IconButton onClick={handleClickIcon}>
-              <ArrowBackIcon />
-            </IconButton>
-          </Link>
+          </Button>
         </Grid>
       </Grid>
       <div>
@@ -120,25 +113,25 @@ export default function UserDetail() {
         <Grid item xs={2}><h3>Total Post:{userData.totalPosts == null ? 0 : userData.totalPosts.count}</h3></Grid>
       </div>
       <GridList cellHeight={400} cols={4}>
-        {userData.totalPosts==null?'':userData.totalPosts.rows.map((post) => userPost(post, classes))}
+        {userData.totalPosts == null ? '' : userData.totalPosts.rows.map((post) => userPost(post, classes))}
       </GridList>
     </Container>
   );
 }
 
 function userPost(post, classes) {
-  const urlToPost = "/posts/"+post.id;
+  const urlToPost = "/posts/" + post.id;
   return (
     <GridListTile key={post.id}>
       <img src={post.imageUrl} alt={post.id} />
-      <GridListTileBar 
+      <GridListTileBar
         title={post.postName}
         subtitle={<span>Time needed:{post.timeNeeded}'</span>}
         actionIcon={
           <Link to={urlToPost}>
-          <IconButton aria-label={`info about ${post.postName}`} className={classes.icon}>
-            <InfoIcon />
-          </IconButton>
+            <IconButton aria-label={`info about ${post.postName}`} className={classes.icon}>
+              <InfoIcon />
+            </IconButton>
           </Link>
         }
       />
